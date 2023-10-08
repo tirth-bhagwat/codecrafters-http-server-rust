05eb72b276a54fcd636e36fa1e12a73108ac064d
tirth-bhagwat <tirth.bhagwat@proton.me>
2023-10-08 11:38:57 +0530

    Stage-4 Respond with content

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,24 @@ fn main() {
             Ok(mut _stream) => {
                 let mut data = vec![0; 512];
                 _stream.read(&mut data).unwrap();
-                if String::from_utf8(data).unwrap().starts_with("GET / HTTP/1.1") {
-                    _stream.write(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
+                let request: String = String::from_utf8(data)
+                    .unwrap()
+                    .split("\r\n")
+                    .into_iter()
+                    .take(1)
+                    .collect();
+
+                // println!("{}", request);
+                // println!("{:?}", request);
+                if request.starts_with("GET /echo/") && request.ends_with(" HTTP/1.1") {
+                    let req_len = request.len();
+                    let resp = format!(
+                        "HTTP/1.1 200 OK\r\n\r\nContent-Type: text/plain\r\nContent-Length: {}\n\r\r\n{}\r\n",
+                        req_len - 19,
+                        request.get(10..req_len - 9).unwrap()
+                    );
+                    println!("op: \n{}", resp);
+                    _stream.write(resp.as_bytes()).unwrap();
                 } else {
                     _stream.write(b"HTTP/1.1 404 Not Found\r\n\r\n").unwrap();
                 }
